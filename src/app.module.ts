66fc262d413ca30ce4b48c30e83efa2ee8f9361a
Vitalii Povzner <vitaliypovzner@gmail.com>
2024-11-19 18:05:04 +0200

    added config module for envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
@@ -12,15 +13,22 @@ import { UserGroupController } from './user-groups/user-group.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'user',
-      password: 'password',
-      database: 'mydatabase',
-      entities: [User, Group, UserGroup],
-      synchronize: false, 
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [User, Group, UserGroup],
+        synchronize: false, 
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([User, Group, UserGroup]),
   ],
